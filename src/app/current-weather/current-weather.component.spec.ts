232e72ea9e21af4839c6d64d3a53e6eb5fdd6946
bonannogiovanni <bonannogiovanni@hotmail.com>
2021-04-20 22:04:22 +0200

    commit

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,25 +1,71 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { WeatherService } from './../weather/weather.service'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { CurrentWeatherComponent } from './current-weather.component';
+import { CurrentWeatherComponent } from './current-weather.component'
+import { injectSpy } from 'angular-unit-test-helper'
+import { of } from 'rxjs'
+import { By } from '@angular/platform-browser'
+import { fakeWeather } from '../weather/weather.service.fake'
 
 describe('CurrentWeatherComponent', () => {
-  let component: CurrentWeatherComponent;
-  let fixture: ComponentFixture<CurrentWeatherComponent>;
+  let component: CurrentWeatherComponent
+  let fixture: ComponentFixture<CurrentWeatherComponent>
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>
 
   beforeEach(async () => {
+    const weatherServiceSpy = jasmine.createSpyObj('WeatherService', [
+      'getCurrentWeather',
+    ])
+
     await TestBed.configureTestingModule({
-      declarations: [ CurrentWeatherComponent ]
-    })
-    .compileComponents();
-  });
+      declarations: [CurrentWeatherComponent],
+      providers: [
+        {
+          provide: WeatherService,
+          useValue: weatherServiceSpy,
+        },
+      ],
+
+      imports: [],
+    }).compileComponents()
+    weatherServiceMock = injectSpy(WeatherService)
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CurrentWeatherComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(CurrentWeatherComponent)
+    component = fixture.componentInstance
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of())
+    // Act
+    fixture.detectChanges()
+    // Assert
+    expect(component).toBeTruthy()
+  })
+
+  it('should get currentWeather from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of())
+    // Act
+    fixture.detectChanges() // triggers ngOnInit()
+    // Assert
+    expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1)
+  })
+
+  it('should eagerly load currentWeather in Bethesda from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather))
+    // Act
+    fixture.detectChanges() // triggers ngOnInit()
+    // Assert
+    expect(component.current).toBeDefined()
+    expect(component.current.city).toEqual('Bethesda')
+    expect(component.current.temperature).toEqual(280.32)
+    // Assert on DOM
+    const debugEl = fixture.debugElement
+    const titleEl: HTMLElement = debugEl.query(By.css('span')).nativeElement
+    expect(titleEl.textContent).toContain('Bethesda')
+  })
+})
